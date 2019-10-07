f5692957769f67e73cf948fc88a68d47aa569a0d
shernk <31926953+shernk@users.noreply.github.com>
2019-10-07 20:16:13 +0700

    finished frontend connected to backend

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -4,7 +4,7 @@ let Exercise = require("../models/exercise.model");
 router.route("/").get((req, res) => {
   Exercise.find()
     .then(Exercises => res.json(Exercises))
-    .catch(err => res.status(400).json("Error" + err));
+    .catch(err => res.status(400).json("Error: " + err));
 });
 
 router.route("/add").post((req, res) => {
@@ -18,7 +18,7 @@ router.route("/add").post((req, res) => {
   newExercise
     .save()
     .then(() => res.json("Exercise Added!"))
-    .catch(err => res.status(400).json("Error" + err));
+    .catch(err => res.status(400).json("Error: " + err));
 });
 
 router.route("/:id").get((req, res) => {
@@ -29,7 +29,7 @@ router.route("/:id").get((req, res) => {
 
 router.route("/:id").delete((req, res) => {
   Exercise.findByIdAndDelete(req.params.id)
-    .then(() => res.json("Exercise deleted."))
+    .then(() => res.json("Exercise deleted!"))
     .catch(err => res.status(400).json("Error: " + err));
 });
 
